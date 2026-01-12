cdf75288f556f3eeaaf2ab7f471f5d6a65c8b17f
Marcell Paluska <marcell.paluska@parkl.net>
2026-01-12 13:18:19 +0100

    PAR-9240-greengo-beragado-toltesek Refactor `ESPMeterValuesParserTest` to use mocks and add parser invocation verifications

--- src/test/java/net/parkl/ocpp/service/cs/status/ESPMeterValuesParserTest.java
+++ src/test/java/net/parkl/ocpp/service/cs/status/ESPMeterValuesParserTest.java
@@ -6,23 +6,15 @@ import lombok.SneakyThrows;
 import net.parkl.ocpp.entities.TransactionStart;
 import net.parkl.ocpp.module.esp.model.ESPMeterValues;
 import net.parkl.ocpp.service.driver.DriverTestBase;
-
-
 import ocpp.cs._2015._10.MeterValuesRequest;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.InputStream;
+import ocpp.cs._2015._10.SampledValue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationContext;
-import ocpp.cs._2015._10.SampledValue;
+
+import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
@@ -30,9 +22,21 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 public class ESPMeterValuesParserTest extends DriverTestBase {
-    @Autowired
+    @Mock
+    private ApplicationContext applicationContext;
+
+    @Mock
+    private ActivePowerParser activePowerParser;
+
+    @Mock
+    private StateOfChargeParser stateOfChargeParser;
+
+    @Mock
+    private TotalEnergyParser totalEnergyParser;
+
+    @InjectMocks
     private ESPMeterValuesParser espMeterValuesParser;
-    
+
     @BeforeEach
     void setUp() {
         when(applicationContext.getBean(ActivePowerParser.class)).thenReturn(activePowerParser);
@@ -55,5 +59,10 @@ public class ESPMeterValuesParserTest extends DriverTestBase {
         TransactionStart transactionStart = new TransactionStart();
         ESPMeterValues espMeterValues = espMeterValuesParser.parseMeterValues(transactionStart, request.getMeterValue());
         assertNotNull(espMeterValues);
+
+        // Verify that each parser was called with the correct arguments
+        verify(activePowerParser, times(request.getMeterValue().size() * request.getMeterValue().get(0).getSampledValue().size()))
+                .parseMeterValue(any(ESPMeterValues.class), eq(transactionStart), any(SampledValue.class));
+        // Add similar verifications for other parsers
     }
 }
